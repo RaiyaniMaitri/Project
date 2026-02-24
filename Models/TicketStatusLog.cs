c609a81147a9edf2d167b85ee2018784efe35901
Maitree <maitriraiyani7@gmail.com>
2026-02-24 11:31:12 +0530

    Add files via upload

---
+++ Models/TicketStatusLog.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class TicketStatusLog
+    {
+        public int Id { get; set; }
+
+        public int TicketId { get; set; }
+        public Ticket? Ticket { get; set; }
+
+        public string OldStatus { get; set; } = string.Empty;
+
+        public string NewStatus { get; set; } = string.Empty;
+
+        public int ChangedBy { get; set; }
+        public User? ChangedByUser { get; set; }
+
+        public DateTime ChangedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        public int RoleId { get; set; }
+        public Role? Role { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
