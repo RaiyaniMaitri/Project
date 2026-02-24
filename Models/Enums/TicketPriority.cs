cd5643cffc6d561889bb367118027219b361f525
Maitree <maitriraiyani7@gmail.com>
2026-02-24 11:32:04 +0530

    Add files via upload

---
+++ Models/Enums/TicketPriority.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Models.Enums
+{
+    public enum TicketPriority
+    {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+}
---
+++ Models/Enums/TicketStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Backend.Models.Enums
+{
+    public enum TicketStatus
+    {
+        OPEN,
+        IN_PROGRESS,
+        RESOLVED,
+        CLOSED
+    }
+}
